152c4d918df4109a2d4f3734c80c1326404d0344
Oleg Khomyakov <homyakov46rus@yandex.ru>
2018-10-15 15:50:54 +0300

    add all button

--- transmiter-base/transmiter-base.ino
+++ transmiter-base/transmiter-base.ino
@@ -1,8 +1,12 @@
 #include "RemoteSwitch.h"
 
-#define BTNON  177877
-#define BTNOFF 177903
-#define PERIOD 263; 
+#define BTNA  177877
+#define BTNB 177903
+#define BTNC 177885
+#define BTND 177886
+#define BTNE 177879
+#define BTNF 177904
+#define PERIOD 263;
 
 
 void setup()
@@ -13,27 +17,16 @@ void setup()
 
 void loop()
 {
-transmit(BTNON);
-Serial.println("otpravleno");
-delay(10);
-transmit(BTNOFF);
-delay(10);
-transmit(177885);
-delay(10);
-transmit(177886);
-delay(10);
-transmit(177879);
-delay(10);
-transmit(177904);
-delay(10);
-
-} 
+  transmit(BTNA);
+  Serial.println("Sended");
+  delay(1000);
+}
 
 
-void transmit(unsigned long rcode){
-unsigned long code = rcode;
-unsigned long period = PERIOD;
-code |= (unsigned long)period << 23;
-code |= 3L << 20; //(|= 3L) 
-RemoteSwitch::sendTelegram(code, 9); // 9 - пин радио передатчика
+void transmit(unsigned long rcode) {
+  unsigned long code = rcode;
+  unsigned long period = PERIOD;
+  code |= (unsigned long)period << 23;
+  code |= 3L << 20; //(|= 3L)
+  RemoteSwitch::sendTelegram(code, 9); // 9 - пин радио передатчика
 }
