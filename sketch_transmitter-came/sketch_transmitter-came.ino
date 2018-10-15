9e40ecdeb5757fff5508465bea9c09a620edbfc1
DenDodge <nestruev98@mail.ru>
2018-10-15 18:46:26 +0300

    fix transmitter-came

--- sketch_transmitter-came/sketch_transmitter-came.ino
+++ sketch_transmitter-came/sketch_transmitter-came.ino
@@ -1,6 +1,9 @@
-// 686 - ворота права(М.Горького 70)
-// 3237 -  вротота сверху(М.Горького 70)
 #define txPin 8 //пин передатчик
+#define startSignal 320
+#define endSignal 11520
+#define longSignal 640
+#define shortSignal 320
+
 void setup()
 {
   pinMode(txPin, OUTPUT);
@@ -9,42 +12,47 @@ void setup()
   Serial.println("Came started");
 }
 
-volatile static long lastCode = 0;
-
-void loop() 
+void loop()
 {
   for (int cod = 0; cod < 4096; cod++)
-  {  
-  SendCame4(3237);
-  delay(300); // меньше 300 не работает
-  Serial.println(cod);
+  {
+    SendCame4(cod);
+    delay(300); // меньше 300 не работает
+    Serial.println(cod);
+    // однократная отправка кода
+    // SendCame4();
   }
 }
 
 
 // функция отправляет код в эфир
-void SendCame4(long Code) {
-  for (int j = 0; j < 7; j++) { // посылку посылаем 4 раза подряд.
+void SendCame4(long Code) 
+{
+  for (int j = 0; j < 7; j++) // посылку посылаем 4 раза подряд.
+  { 
     // время стартового импульса
     digitalWrite(txPin, HIGH);
-    delayMicroseconds(320);
+    delayMicroseconds(startSignal);
     digitalWrite(txPin, LOW);
-    for (int i = 12; i > 0; i--) {
+    for (int i = 12; i > 0; i--) 
+    {
       byte b = bitRead(Code, i - 1); // побитово перебираем и посылаем код
-      if (b) {
+      if (b) 
+      {
         digitalWrite(txPin, LOW); // 1
-        delayMicroseconds(640);
+        delayMicroseconds(longSignal);
         digitalWrite(txPin, HIGH);
-        delayMicroseconds(320);
+        delayMicroseconds(shortSignal);
       }
-      else {
+      else 
+      {
         digitalWrite(txPin, LOW); // 0
-        delayMicroseconds(320);
+        delayMicroseconds(shortSignal);
         digitalWrite(txPin, HIGH);
-        delayMicroseconds(640);
+        delayMicroseconds(longSignal);
       }
     }
     digitalWrite(txPin, LOW);
-    delayMicroseconds(11520);
+    delayMicroseconds(endSignal);
   }
 }
